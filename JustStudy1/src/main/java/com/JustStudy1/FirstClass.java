84b25dbce6e36d721d701ac49ce954d3dcd5b0fd
Administrator <Administrator@DELL>
2019-02-06 20:34:39 +0530

    Newly added contents to FirstClass.

--- JustStudy1/src/main/java/com/JustStudy1/FirstClass.java
+++ JustStudy1/src/main/java/com/JustStudy1/FirstClass.java
@@ -3,5 +3,6 @@ package com.JustStudy1;
 public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Contents of FirstClass.");
+		System.out.println("New contents added to FirstClass.");
 	}
 }
