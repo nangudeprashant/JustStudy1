d790d2fbfe236c54cd33c08c4a276f4389f27360
Administrator <Administrator@DELL>
2019-02-08 18:52:37 +0530

    New line added....

--- JustStudy1/src/main/java/com/JustStudy1/NewJavaClass.java
+++ JustStudy1/src/main/java/com/JustStudy1/NewJavaClass.java
@@ -3,6 +3,7 @@ package com.JustStudy1;
 public class NewJavaClass {
 	void display() {
 		System.out.println("These are the contents of NewJavaClass.");
+		System.out.println("New line added.");
 	}
 
 }
